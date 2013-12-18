528216ddafacf85b40e8d1231e068e544cdad8f5
jaymz <jaymz@u-dox.com>
2013-12-18 12:01:20 +0000

    adding support for a product card, add docstrings

--- twittercard/templatetags/twittercard.py
+++ twittercard/templatetags/twittercard.py
@@ -2,24 +2,18 @@ from django import template
 from django.conf import settings
 register = template.Library()
 
-def twittercard_summary(context, *args, **kwargs):
-    card = get_twittercard_attributes(kwargs)
-    card['card'] = 'summary'
-    request = context['request']
-    card['url'] = kwargs.get('url', request.build_absolute_uri())
-    return card
-register.inclusion_tag('twittercard/summary.html', takes_context=True)(twittercard_summary)
 
-def twittercard_photo(context, *args, **kwargs):
-    card = get_twittercard_attributes(kwargs)
-    card['card'] = 'photo'
-    card['image_width'] = kwargs.get('image_width', None)
-    card['image_height'] = kwargs.get('image_height', None)
-    return card
-register.inclusion_tag('twittercard/photo.html', takes_context=True)(twittercard_photo)
+def get_twittercard_attributes(card_type, **kwargs):
+    """
+        Create a dictionary with our base card data
 
-def get_twittercard_attributes(kwargs):
+        :param card: the name/type of card
+        :param kwargs: meta attributes for this card
+        :type card: str
+        :type kwargs: dict
+    """
     card = {}
+    card['card'] = card_type
     card['title'] = kwargs.get('title', None)
     card['description'] = kwargs.get('description', None)
     card['image'] = kwargs.get('image', None)
@@ -30,3 +24,36 @@ def get_twittercard_attributes(kwargs):
         card['creator'] = kwargs.get('creator', config.get('CREATOR', None))
         card['creator_id'] = kwargs.get('creator_id', config.get('CREATOR_ID', None))
     return card
+
+
+def twittercard_summary(context, *args, **kwargs):
+    """
+        A summary card - the standard one
+    """
+    card = get_twittercard_attributes('summary', **kwargs)
+    request = context['request']
+    card['url'] = kwargs.get('url', request.build_absolute_uri())
+    return card
+register.inclusion_tag('twittercard/summary.html', takes_context=True)(twittercard_summary)
+
+
+def twittercard_photo(context, *args, **kwargs):
+    """
+        A photo card
+    """
+    card = get_twittercard_attributes('photo', **kwargs)
+    card['image_width'] = kwargs.get('image_width', None)
+    card['image_height'] = kwargs.get('image_height', None)
+    return card
+register.inclusion_tag('twittercard/photo.html', takes_context=True)(twittercard_photo)
+
+
+def twittercard_product(context, *args, **kwargs):
+    """
+        A product specific card including price data
+    """
+    card = get_twittercard_attributes('product', **kwargs)
+    card['price'] = kwargs.get('price', None)
+    card['location'] = kwargs.get('location', None)
+    return card
+register.inclusion_tag('twittercard/product.html', takes_context=True)(twittercard_product)
